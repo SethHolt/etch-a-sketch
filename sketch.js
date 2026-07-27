96dcdc4871e6b7cce8148bc624fa55f550b17dc0
SethHolt <josephmarc@sethholt.co.uk>
2026-07-27 19:07:18 +0100

    etch-a-sketch working with color changing on mouseover

--- sketch.js
+++ sketch.js
@@ -3,5 +3,9 @@ const grid = document.getElementById('container');
 for (let i = 0; i < 256; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
+    cell.addEventListener('mouseover', () => {
+        cell.classList.toggle('active');
+    });
     grid.appendChild(cell);
 }
+
