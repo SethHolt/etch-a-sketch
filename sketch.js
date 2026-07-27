e6849b1f9371be673f4166c3b36df5dfbb1cdc63
SethHolt <josephmarc@sethholt.co.uk>
2026-07-27 20:47:33 +0100

    added a progressive darkening effect using opacity in CSS.

--- sketch.js
+++ sketch.js
@@ -12,20 +12,25 @@ button.addEventListener('click', () => {
     grid.style.height = `${gridSize * 40}px`
     grid.style.width = `${gridSize * 40}px`
     generateGrid(gridSize);
-    
 })
 
 function generateGrid(gridSize) {for (let i = 0; i < gridSize * gridSize; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
     cell.addEventListener('mouseover', () => {
+        let opacity = parseFloat(cell.dataset.opacity) || 0;
+        opacity = Math.min(opacity + 0.1, 1);
+        cell.dataset.opacity = opacity;
+
         cell.style.backgroundColor = `rgb(${Math.round(Math.random() * 255)},
                                           ${Math.round(Math.random() * 255)},
                                           ${Math.round(Math.random() * 255)}`;
+
+        cell.style.opacity = opacity;
+        if (opacity === 1) cell.style.backgroundColor = '#000';
     });
     grid.appendChild(cell);
     }
-
 }
 
 generateGrid(gridSize);
